aa3e680107db6184c78e66ca368b720b2723d132
take-iwiw <take.iwiw2222@gmail.com>
2020-06-13 20:06:10 +0900

    add prebuilt libraries for VS2017

--- third_party/cmakes/MNN.cmake
+++ third_party/cmakes/MNN.cmake
@@ -6,14 +6,17 @@ target_include_directories(${PROJECT_NAME} PUBLIC ${MNN_DIR}/3rd_party/imageHelp
 set(USE_PREBUILT_MNN on CACHE BOOL "Use Prebuilt MNN? [on/off]")
 if(USE_PREBUILT_MNN)
 	if(MSVC_VERSION)
-		file(COPY ${CMAKE_CURRENT_LIST_DIR}/../MNN_prebuilt/x64_windows/MNN.dll DESTINATION ${CMAKE_CURRENT_BINARY_DIR})
+		file(COPY ${CMAKE_CURRENT_LIST_DIR}/../MNN_prebuilt/x64_windows/VS2017/Debug/MNN.dll DESTINATION ${CMAKE_CURRENT_BINARY_DIR}/Debug)
+		file(COPY ${CMAKE_CURRENT_LIST_DIR}/../MNN_prebuilt/x64_windows/VS2017/RelWithDebInfo/MNN.dll DESTINATION ${CMAKE_CURRENT_BINARY_DIR}/RelWithDebInfo)
+		file(COPY ${CMAKE_CURRENT_LIST_DIR}/../MNN_prebuilt/x64_windows/VS2017/Release/MNN.dll DESTINATION ${CMAKE_CURRENT_BINARY_DIR}/Release)
+		file(COPY ${CMAKE_CURRENT_LIST_DIR}/../MNN_prebuilt/x64_windows/VS2017/MinSizeRel/MNN.dll DESTINATION ${CMAKE_CURRENT_BINARY_DIR}/MinSizeRel)
 		# file(COPY ${CMAKE_CURRENT_LIST_DIR}/../MNN_prebuilt/x64_windows/MNNd.dll DESTINATION ${CMAKE_CURRENT_BINARY_DIR})
 		if((MSVC_VERSION GREATER_EQUAL 1910) AND (MSVC_VERSION LESS 1920))
 			target_link_libraries(${PROJECT_NAME}
-				$<$<CONFIG:Debug>:${CMAKE_CURRENT_LIST_DIR}/../MNN_prebuilt/x64_windows/MNN.lib>
-				$<$<CONFIG:RelWithDebInfo>:${CMAKE_CURRENT_LIST_DIR}/../MNN_prebuilt/x64_windows/MNN.lib>
-				$<$<CONFIG:Release>:${CMAKE_CURRENT_LIST_DIR}/../MNN_prebuilt/x64_windows/MNN.lib>
-				$<$<CONFIG:MinSizeRel>:${CMAKE_CURRENT_LIST_DIR}/../MNN_prebuilt/x64_windows/MNN.lib>
+				$<$<CONFIG:Debug>:${CMAKE_CURRENT_LIST_DIR}/../MNN_prebuilt/x64_windows/VS2017/Debug/MNN.lib>
+				$<$<CONFIG:RelWithDebInfo>:${CMAKE_CURRENT_LIST_DIR}/../MNN_prebuilt/x64_windows/VS2017/RelWithDebInfo/MNN.lib>
+				$<$<CONFIG:Release>:${CMAKE_CURRENT_LIST_DIR}/../MNN_prebuilt/x64_windows/VS2017/Release/MNN.lib>
+				$<$<CONFIG:MinSizeRel>:${CMAKE_CURRENT_LIST_DIR}/../MNN_prebuilt/x64_windows/VS2017/MinSizeRel/MNN.lib>
 			)
 		else()
 			message(FATAL_ERROR "[MNN] unsupported MSVC version")
