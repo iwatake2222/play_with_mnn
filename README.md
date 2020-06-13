5f5180286f5b25018209db9b73df007e028a8f90
take-iwiw <take.iwiw2222@gmail.com>
2020-06-14 00:54:11 +0900

    organize sample project. add readme

---
+++ README.md
@@ -0,0 +1,128 @@
+# Play with MNN
+Sample projects to use MNN (https://github.com/alibaba/MNN )
+
+## Target environment
+- Windows(MSVC) (x64)
+- Linux (x64)
+- Linux (armv7) e.g. Raspberry Pi 3,4
+- Linux (aarch64) e.g. Jetson Nano
+- *Native build only (Cross build is not supported)
+
+
+## How to build application code
+```
+cd play_with_mnn
+git submodule init
+git submodule update
+
+cd pj_mnn_cls_mobilenet_v2
+mkdir build && cd build
+cmake ..
+make
+
+./main
+```
+
+If you use Visual Studio, please use cmake-gui to generate project files.
+
+## How to create pre-built MNN library
+pre-built MNN library is stored in third_party/MNN_prebuilt . Please use the following commands if you want to build them by yourself.
+
+
+### Linux
+```
+cd third_party/MNN/schema
+./generate.sh
+cd ../../../
+
+cd pj_mnn_cls_mobilenet_v2
+mkdir build && cd build
+cmake .. -DUSE_PREBUILT_MNN=off
+make
+
+./main
+```
+
+### Windows (Visual Studio)
+- Build `third_party\MNN\3rd_party\flatbuffers` in Visual Studio (use cmake-gui)
+- Copy flatc.exe to any place (Prebuilt executable file is stored in `play_with_mnn\third_party\MNN_prebuilt\tools\x64_windows\flatc.exe`
+- Modify `play_with_mnn\third_party\MNN\schema\generate.ps1` not to build flatc.exe and use the created flatc.exe
+	```
+	+++ b/schema/generate.ps1
+	@@ -9,7 +9,7 @@ if (($args[0] -eq "-lazy") -and ( Test-Path "current" -PathType Container )) {
+	}
+
+	# check is flatbuffer installed or not
+	-Set-Variable -Name "FLATC" -Value "..\3rd_party\flatbuffers\tmp\flatc.exe"
+	+Set-Variable -Name "FLATC" -Value "..\..\MNN_prebuilt\tools\x64_windows\flatc.exe"
+	if (-Not (Test-Path $FLATC -PathType Leaf)) {
+	echo "*** building flatc ***"
+
+	@@ -21,8 +21,8 @@ if (-Not (Test-Path $FLATC -PathType Leaf)) {
+	(cd tmp) -and (rm -r -force *)
+
+	# build
+	-  cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release ..
+	-  cmake --build . --target flatc
+	+  # cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release ..
+	+  # cmake --build . --target flatc
+
+	# dir recover
+	popd
+	```
+- Run the modified `generate.ps` in PowerShell
+	- You may need `Set-ExecutionPolicy -Scope CurrentUser RemoteSigned`
+- Create a Visual Studio project using cmake-gui
+- Build the project in Visual Studio
+
+
+## How to create a model converter tool
+### Linux
+- Install protobuf
+	```
+	cd ~/
+	git clone https://github.com/protocolbuffers/protobuf.git
+	cd protobuf
+	git checkout  v3.7.1
+	./autogen.sh
+	./configure CXXFLAGS="-fPIC" --prefix=/usr/local --disable-shared 2>&1 > /dev/null
+	make -j2
+	sudo make install
+	sudo ldconfig
+	```
+- Build MNN with the following option
+	- `cmake .. -DUSE_PREBUILT_MNN=off -DMNN_BUILD_QUANTOOLS=on -DMNN_BUILD_CONVERTER=on `
+- (in my environment, the converter with `-DMNN_BUILD_SHARED_LIBS=off` causes segmentation fault...)
+
+### Windows(Visual Studio)
+- Install protobuf
+	```
+	cd ~/
+	git clone https://github.com/protocolbuffers/protobuf.git
+	cd protobuf
+	git checkout  v3.7.1
+	```
+	- Create a Visual Studio project for protobuf usin cmake-gui
+	- Build protobuf and install
+		- Set Runtime library in Code Generation in property as Multithread (/MT)
+- Create a Visual Studio project for MNN using cmake-gui
+	- MNN_BUILD_SHARED_LIBS=off
+	- MNN_BUILD_QUANTOOLS=on
+	- MNN_BUILD_CONVERTER=on
+	- Fill out protobuflib settings
+		
+		![protobuflib](00_doc/windows_tool.png) 
+- Build the project in Visual Studio
+
+# Acknowledgements
+- This project includes the work that is distributed in the Apache License 2.0.
+- This project includes MNN (https://github.com/alibaba/MNN ) as a submodule
+- The models are retrieved from the followings:
+	- mobilenet_v2_1.0_224.tflite
+		- https://www.tensorflow.org/lite/guide/hosted_models
+		- https://storage.googleapis.com/download.tensorflow.org/models/tflite_11_05_08/mobilenet_v2_1.0_224.tgz
+		- `MNNConvert.exe -f TFLITE --modelFile mobilenet_v2_1.0_224.tflite --MNNModel mobilenet_v2_1.0_224.mnn --bizCode biz`
+	- posenet-mobilenet_v1_075.pb
+		- https://github.com/czy2014hust/posenet-python
+		- https://github.com/czy2014hust/posenet-python/blob/master/models/model-mobilenet_v1_075.pb?raw=true
+		- `MNNConvert.exe -f TF --modelFile posenet-mobilenet_v1_075.pb --MNNModel posenet-mobilenet_v1_075.mnn --bizCode biz`
--- pj_mnn_cls_mobilenet_v2/Main.cpp
+++ pj_mnn_cls_mobilenet_v2/Main.cpp
@@ -1,155 +1,121 @@
-//
-//  pictureRecognition.cpp
-//  MNN
-//
-//  Created by MNN on 2018/05/14.
-//  Copyright © 2018, Alibaba Group Holding Limited
-//
-
+/*** Include ***/
+/* for general */
 #include <stdio.h>
-#include <MNN/ImageProcess.hpp>
-#include <MNN/Interpreter.hpp>
-#define MNN_OPEN_TIME_TRACE
 #include <algorithm>
 #include <fstream>
 #include <functional>
 #include <memory>
 #include <sstream>
 #include <vector>
+#include <chrono>
+
+/* for OpenCV */
+#include <opencv2/opencv.hpp>
+
+/* for MNN */
+#include <MNN/ImageProcess.hpp>
+#include <MNN/Interpreter.hpp>
 #include <MNN/AutoTime.hpp>
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
-#include "stb_image_write.h"
 
-using namespace MNN;
-using namespace MNN::CV;
+/*** Macro ***/
+/* Model parameters */
+#define MODEL_NAME   RESOURCE_DIR"/model/mobilenet_v2_1.0_224.mnn"
+#define IMAGE_NAME   RESOURCE_DIR"/parrot.jpg"
 
-int main(int argc, const char* argv[]) {
-    if (argc < 3) {
-        MNN_PRINT("Usage: ./pictureTest.out model.mnn input.jpg [word.txt]\n");
-        return 0;
-    }
-    std::shared_ptr<Interpreter> net(Interpreter::createFromFile(argv[1]));
-    ScheduleConfig config;
-    config.type  = MNN_FORWARD_AUTO;
-    // BackendConfig bnconfig;
-    // bnconfig.precision = BackendConfig::Precision_Low;
-    // config.backendConfig = &bnconfig;
-    auto session = net->createSession(config);
+/* Settings */
+#define LOOP_NUM_FOR_TIME_MEASUREMENT 100
 
-    auto input = net->getSessionInput(session, NULL);
-    auto shape = input->shape();
-    shape[0]   = 1;
-    net->resizeTensor(input, shape);
-    net->resizeSession(session);
-    auto output = net->getSessionOutput(session, NULL);
-    std::vector<std::string> words;
-    if (argc >= 4) {
-        std::ifstream inputOs(argv[3]);
-        std::string line;
-        while (std::getline(inputOs, line)) {
-            words.emplace_back(line);
-        }
-    }
-    {
-        auto dims    = input->shape();
-        int inputDim = 0;
-        int size_w   = 0;
-        int size_h   = 0;
-        int bpp      = 0;
-        bpp          = input->channel();
-        size_h       = input->height();
-        size_w       = input->width();
-        if (bpp == 0)
-            bpp = 1;
-        if (size_h == 0)
-            size_h = 1;
-        if (size_w == 0)
-            size_w = 1;
-        MNN_PRINT("input: w:%d , h:%d, bpp: %d\n", size_w, size_h, bpp);
 
-        auto inputPatch = argv[2];
-        int width, height, channel;
-        auto inputImage = stbi_load(inputPatch, &width, &height, &channel, 4);
-        if (nullptr == inputImage) {
-            MNN_ERROR("Can't open %s\n", inputPatch);
-            return 0;
-        }
-        MNN_PRINT("origin size: %d, %d\n", width, height);
-        Matrix trans;
-        // Set transform, from dst scale to src, the ways below are both ok
-#ifdef USE_MAP_POINT
-        float srcPoints[] = {
-            0.0f, 0.0f,
-            0.0f, (float)(height-1),
-            (float)(width-1), 0.0f,
-            (float)(width-1), (float)(height-1),
-        };
-        float dstPoints[] = {
-            0.0f, 0.0f,
-            0.0f, (float)(size_h-1),
-            (float)(size_w-1), 0.0f,
-            (float)(size_w-1), (float)(size_h-1),
-        };
-        trans.setPolyToPoly((Point*)dstPoints, (Point*)srcPoints, 4);
-#else
-        trans.setScale((float)(width-1) / (size_w-1), (float)(height-1) / (size_h-1));
-#endif
-        ImageProcess::Config config;
-        config.filterType = BILINEAR;
-        float mean[3]     = {103.94f, 116.78f, 123.68f};
-        float normals[3] = {0.017f, 0.017f, 0.017f};
-        // float mean[3]     = {127.5f, 127.5f, 127.5f};
-        // float normals[3] = {0.00785f, 0.00785f, 0.00785f};
-        ::memcpy(config.mean, mean, sizeof(mean));
-        ::memcpy(config.normal, normals, sizeof(normals));
-        config.sourceFormat = RGBA;
-        config.destFormat   = BGR;
+int main(int argc, const char* argv[])
+{
+	/*** Initialize ***/
+	/* Create interpreter */
+	std::shared_ptr<MNN::Interpreter> net(MNN::Interpreter::createFromFile(MODEL_NAME));
+	MNN::ScheduleConfig scheduleConfig;
+	scheduleConfig.type  = MNN_FORWARD_AUTO;
+	scheduleConfig.numThread = 4;
+	// BackendConfig bnconfig;
+	// bnconfig.precision = BackendConfig::Precision_Low;
+	// config.backendConfig = &bnconfig;
+	auto session = net->createSession(scheduleConfig);
 
-        std::shared_ptr<ImageProcess> pretreat(ImageProcess::create(config));
-        pretreat->setMatrix(trans);
-        pretreat->convert((uint8_t*)inputImage, width, height, 0, input);
-        stbi_image_free(inputImage);
-    }
-    net->runSession(session);
-    {
-        auto dimType = output->getDimensionType();
-        if (output->getType().code != halide_type_float) {
-            dimType = Tensor::TENSORFLOW;
-        }
-        std::shared_ptr<Tensor> outputUser(new Tensor(output, dimType));
-        MNN_PRINT("output size:%d\n", outputUser->elementSize());
-        output->copyToHostTensor(outputUser.get());
-        auto type = outputUser->getType();
+	/* Get model information */
+	auto input = net->getSessionInput(session, NULL);
+	int modelChannel = input->channel();
+	int modelHeight  = input->height();
+	int modelWidth   = input->width();
+	printf("model input size: widgh = %d , height = %d, channel = %d\n", modelWidth, modelHeight, modelChannel);
 
-        auto size = outputUser->elementSize();
-        std::vector<std::pair<int, float>> tempValues(size);
-        if (type.code == halide_type_float) {
-            auto values = outputUser->host<float>();
-            for (int i = 0; i < size; ++i) {
-                tempValues[i] = std::make_pair(i, values[i]);
-            }
-        }
-        if (type.code == halide_type_uint && type.bytes() == 1) {
-            auto values = outputUser->host<uint8_t>();
-            for (int i = 0; i < size; ++i) {
-                tempValues[i] = std::make_pair(i, values[i]);
-            }
-        }
-        // Find Max
-        std::sort(tempValues.begin(), tempValues.end(),
-                  [](std::pair<int, float> a, std::pair<int, float> b) { return a.second > b.second; });
+	/***** Process for each frame *****/
+	/*** Read image ***/
+	cv::Mat originalImage = cv::imread(IMAGE_NAME);
+	int imageWidth = originalImage.size[1];
+	int imageHeight = originalImage.size[0];
+	printf("image size: width = %d, height = %d\n", imageWidth, imageHeight);
+	
+	/*** Pre process (resize, colorconversion, normalize) ***/
+	MNN::CV::ImageProcess::Config imageProcessconfig;
+	imageProcessconfig.filterType = MNN::CV::BILINEAR;
+	float mean[3]     = {127.5f, 127.5f, 127.5f};
+	float normals[3] = {0.00785f, 0.00785f, 0.00785f};
+	std::memcpy(imageProcessconfig.mean, mean, sizeof(mean));
+	std::memcpy(imageProcessconfig.normal, normals, sizeof(normals));
+	imageProcessconfig.sourceFormat = MNN::CV::BGR;
+	imageProcessconfig.destFormat   = MNN::CV::BGR;
 
-        int length = size > 10 ? 10 : size;
-        if (words.empty()) {
-            for (int i = 0; i < length; ++i) {
-                MNN_PRINT("%d, %f\n", tempValues[i].first, tempValues[i].second);
-            }
-        } else {
-            for (int i = 0; i < length; ++i) {
-                MNN_PRINT("%s: %f\n", words[tempValues[i].first].c_str(), tempValues[i].second);
-            }
-        }
-    }
-    return 0;
+	MNN::CV::Matrix trans;
+	trans.setScale((float)imageWidth/modelWidth, (float)imageHeight/modelHeight);
+
+	std::shared_ptr<MNN::CV::ImageProcess> pretreat(MNN::CV::ImageProcess::create(imageProcessconfig));
+	pretreat->setMatrix(trans);
+	pretreat->convert((uint8_t*)originalImage.data, imageWidth, imageHeight, 0, input);
+
+	/*** Inference ***/
+	net->runSession(session);
+
+	/*** Post process ***/
+	/* Retreive results */
+	auto output = net->getSessionOutput(session, NULL);
+	auto dimType = output->getDimensionType();
+
+	std::shared_ptr<MNN::Tensor> outputUser(new MNN::Tensor(output, dimType));
+	output->copyToHostTensor(outputUser.get());
+	auto size = outputUser->elementSize();
+	auto type = outputUser->getType();
+	printf("output size: size = %d\n", size);
+	
+	std::vector<std::pair<int, float>> tempValues(size);
+	if (type.code == halide_type_float) {
+		auto values = outputUser->host<float>();
+		for (int i = 0; i < size; ++i) {
+			tempValues[i] = std::make_pair(i, values[i]);
+		}
+	} else if (type.code == halide_type_uint && type.bytes() == 1) {
+		auto values = outputUser->host<uint8_t>();
+		for (int i = 0; i < size; ++i) {
+			tempValues[i] = std::make_pair(i, values[i]);
+		}
+	} else {
+		printf("should not reach here\n");
+	}
+
+	/* Find the highest score */
+	std::sort(tempValues.begin(), tempValues.end(), [](std::pair<int, float> a, std::pair<int, float> b) { return a.second > b.second; });
+	int length = size > 10 ? 10 : size;
+	for (int i = 0; i < length; ++i) {
+		printf("%d: %f\n", tempValues[i].first, tempValues[i].second);
+	}
+
+
+	/*** (Optional) Measure inference time ***/
+	const auto& t0 = std::chrono::steady_clock::now();
+	net->runSession(session);
+	const auto& t1 = std::chrono::steady_clock::now();
+	std::chrono::duration<double> timeSpan = t1 - t0;
+	printf("Inference time = %f [msec]\n", timeSpan.count() * 1000.0 / LOOP_NUM_FOR_TIME_MEASUREMENT);
+
+	/*** Finalize ***/
+	net->releaseSession(session);
+
+	return 0;
 }
