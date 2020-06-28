ab97f38097a5051c773f1d78012f7777586db162
take-iwiw <take.iwiw2222@gmail.com>
2020-06-28 16:52:05 +0900

    change the way to import MNN in cmake

--- third_party/cmakes/MNN.cmake
+++ third_party/cmakes/MNN.cmake
@@ -1,17 +1,16 @@
 set(MNN_DIR ${CMAKE_SOURCE_DIR}/../third_party/MNN)
-target_include_directories(${PROJECT_NAME} PUBLIC ${MNN_DIR}/include)
-target_include_directories(${PROJECT_NAME} PUBLIC ${MNN_DIR}/3rd_party/imageHelper)
+set(MNN_INC ${MNN_DIR}/include)
 
 set(USE_PREBUILT_MNN on CACHE BOOL "Use Prebuilt MNN? [on/off]")
 if(USE_PREBUILT_MNN)
 	if(MSVC_VERSION)
-		file(COPY ${CMAKE_CURRENT_LIST_DIR}/../MNN_prebuilt/x64_windows/VS2017/Debug/MNN.dll DESTINATION ${CMAKE_CURRENT_BINARY_DIR}/Debug)
-		file(COPY ${CMAKE_CURRENT_LIST_DIR}/../MNN_prebuilt/x64_windows/VS2017/RelWithDebInfo/MNN.dll DESTINATION ${CMAKE_CURRENT_BINARY_DIR}/RelWithDebInfo)
-		file(COPY ${CMAKE_CURRENT_LIST_DIR}/../MNN_prebuilt/x64_windows/VS2017/Release/MNN.dll DESTINATION ${CMAKE_CURRENT_BINARY_DIR}/Release)
-		file(COPY ${CMAKE_CURRENT_LIST_DIR}/../MNN_prebuilt/x64_windows/VS2017/MinSizeRel/MNN.dll DESTINATION ${CMAKE_CURRENT_BINARY_DIR}/MinSizeRel)
-		# file(COPY ${CMAKE_CURRENT_LIST_DIR}/../MNN_prebuilt/x64_windows/MNNd.dll DESTINATION ${CMAKE_CURRENT_BINARY_DIR})
+		file(COPY ${CMAKE_CURRENT_LIST_DIR}/../MNN_prebuilt/x64_windows/VS2017/Debug/MNN.dll DESTINATION ${CMAKE_BINARY_DIR}/Debug)
+		file(COPY ${CMAKE_CURRENT_LIST_DIR}/../MNN_prebuilt/x64_windows/VS2017/RelWithDebInfo/MNN.dll DESTINATION ${CMAKE_BINARY_DIR}/RelWithDebInfo)
+		file(COPY ${CMAKE_CURRENT_LIST_DIR}/../MNN_prebuilt/x64_windows/VS2017/Release/MNN.dll DESTINATION ${CMAKE_BINARY_DIR}/Release)
+		file(COPY ${CMAKE_CURRENT_LIST_DIR}/../MNN_prebuilt/x64_windows/VS2017/MinSizeRel/MNN.dll DESTINATION ${CMAKE_BINARY_DIR}/MinSizeRel)
+		# file(COPY ${CMAKE_CURRENT_LIST_DIR}/../MNN_prebuilt/x64_windows/MNNd.dll DESTINATION ${CMAKE_BINARY_DIR})
 		if((MSVC_VERSION GREATER_EQUAL 1910) AND (MSVC_VERSION LESS 1920))
-			target_link_libraries(${PROJECT_NAME}
+			set(MNN_LIB
 				$<$<CONFIG:Debug>:${CMAKE_CURRENT_LIST_DIR}/../MNN_prebuilt/x64_windows/VS2017/Debug/MNN.lib>
 				$<$<CONFIG:RelWithDebInfo>:${CMAKE_CURRENT_LIST_DIR}/../MNN_prebuilt/x64_windows/VS2017/RelWithDebInfo/MNN.lib>
 				$<$<CONFIG:Release>:${CMAKE_CURRENT_LIST_DIR}/../MNN_prebuilt/x64_windows/VS2017/Release/MNN.lib>
@@ -21,7 +20,7 @@ if(USE_PREBUILT_MNN)
 			message(FATAL_ERROR "[MNN] unsupported MSVC version")
 		endif()
 	else()
-		target_link_libraries(${ProjectName}
+		set(MNN_LIB
 			# $<$<STREQUAL:${BUILD_SYSTEM},x64_windows>:${CMAKE_SOURCE_DIR}/../third_party/MNN_prebuilt/x64_windows/lib/ncnn.lib>
 			$<$<STREQUAL:${BUILD_SYSTEM},x64_linux>:${CMAKE_SOURCE_DIR}/../third_party/MNN_prebuilt/x64_linux/libMNN.so>
 			$<$<STREQUAL:${BUILD_SYSTEM},armv7>:${CMAKE_SOURCE_DIR}/../third_party/MNN_prebuilt/armv7/libMNN.so>
